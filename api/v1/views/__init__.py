7cfa5890feb8ae153a7285339581b551ea477b81
Agus <agustinmeriles@hotmail.com>
2023-04-11 17:22:33 -0300

    import correction

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Shebang"""
 from flask import Blueprint
-from api.v1.views.index import *
+from api.v1.views import index
 
 
 app_views = Blueprint("app_views",url_prefix="/api/v1")
