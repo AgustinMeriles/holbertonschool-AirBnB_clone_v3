03b6405e02ff4f76cf1f88d72dab55c4f823ae90
Agus <agustinmeriles@hotmail.com>
2023-04-12 19:57:22 -0300

    correction of distract errors

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,3 +1,5 @@
+ #!/usr/bin/python3
+"""new view for State objects that handles all default RESTFul API actions"""
 from flask import jsonify, request, abort
 from api.v1.views import app_views
 from models.state import State
