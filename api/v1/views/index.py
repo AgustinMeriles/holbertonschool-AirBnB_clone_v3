0523a6dfc805e07ba001c21018c322b6511b2f40
NMenendez24 <nahuelmt24@gmail.com>
2023-04-12 16:03:35 -0300

    Change return status to jsonify

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """Shebang"""
 from api.v1.views import app_views
+from flask import jsonify
 
 
-@app_views.route("/status")
+@app_views.route("/status", strict_slashes=False)
 def status():
     """Function that returns a JSON status"""
-    return {
-        "status": "OK"
-        }
+    return jsonify({"status": "OK"})
