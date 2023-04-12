1e9f96c4a46625607baf6964d25454efb6cb647b
NMenendez24 <nahuelmt24@gmail.com>
2023-04-12 12:43:12 -0300

    Some changes in the import statements

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """File appy.py"""
 from models import storage
-from api.v1.views.__init__ import app_views
+from api.v1.views import app_views
 from flask import Flask
 from os import getenv
 
@@ -9,16 +9,20 @@ from os import getenv
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def destroy(exception):
     """Function that closes the API"""
     storage.close()
 
+
 if __name__ == "__main__":
-    host = getenv(HBNB_API_HOST)
-    port = getenv(HBNB_API_PORT)
-    app.run(
-        host="0.0.0.0" if host is None else host,
-        port="5000" if port is None else port,
-        threaded=True
-    )
+    if getenv(HBNB_API_HOST) is None:
+        host = "0.0.0.0"
+    else:
+        host = getenv(HBNB_API_HOST)
+    if getenv(HBNB_API_PORT) is None:
+        port = "5000"
+    else:
+        port = getenv(HBNB_API_PORT)
+    app.run(host=host, port=port, threaded=True)
