6b60d11982cdfaacec994c8c5a64e31e9f763735
NMenendez24 <nahuelmt24@gmail.com>
2023-04-12 13:10:34 -0300

    Change status method

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """Shebang"""
 from api.v1.views import app_views
-from flask import jsonify
 
 
 @app_views.route("/status")
 def status():
     """Function that returns a JSON status"""
-    return jsonify(status="OK")
+    return {'status': 'OK'}
