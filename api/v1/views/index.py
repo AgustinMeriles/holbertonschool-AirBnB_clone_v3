ca12b190d7a6d7308c75bb722a8f9f611d36fba3
NMenendez24 <nahuelmt24@gmail.com>
2023-04-12 13:19:05 -0300

    Change status method

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """Shebang"""
 from api.v1.views import app_views
+from json import dumps
 
 
 @app_views.route("/status")
 def status():
     """Function that returns a JSON status"""
-    return {'status': 'OK'}
+    return dumps({
+        'status': 'OK'
+        })
