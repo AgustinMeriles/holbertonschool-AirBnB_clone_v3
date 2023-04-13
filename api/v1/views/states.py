9ee9c4254cdaefbd97d683cfa3ef1625d4b74ff4
NMenendez24 <nahuelmt24@gmail.com>
2023-04-13 14:47:08 -0300

    Small login change

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -38,7 +38,6 @@ def create_state():
     if 'name' not in data:
         abort(400, 'Missing name')
     state = State(**data)
-    # storage.new(state)
     state.save()
     return jsonify(state.to_dict()), 201
 
