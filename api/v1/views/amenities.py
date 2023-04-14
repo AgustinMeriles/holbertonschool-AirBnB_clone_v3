896c580022867250b7f6449978832799c93cee24
NMenendez24 <nahuelmt24@gmail.com>
2023-04-14 15:28:22 -0300

    Strict Slashes

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -21,7 +21,8 @@ def get_amenity(amenity_id):
     return jsonify(amenities.to_dict())
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['DELETE'])
+@app_views.route('/amenities/<amenity_id>', methods=['DELETE'],
+                 strict_slashes=False)
 def delete_amenity(amenity_id):
     amenities = storage.get(Amenity, amenity_id)
     if not amenities:
@@ -43,7 +44,8 @@ def create_amenity():
     return jsonify(amenities.to_dict()), 201
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['PUT'])
+@app_views.route('/amenities/<amenity_id>', methods=['PUT'],
+                 strict_slashes=False)
 def update_amenity(amenity_id):
     amenities = storage.get(Amenity, amenity_id)
     if not amenities:
