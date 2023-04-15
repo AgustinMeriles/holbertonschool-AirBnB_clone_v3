1a4f2374c0121a2b6d885c0254b75015e834971c
Agus <agustinmeriles@hotmail.com>
2023-04-14 21:59:41 -0300

    places.py update

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -12,17 +12,17 @@ from models.user import User
     '/cities/<city_id>/places', strict_slashes=False, methods=['GET'])
 def get_cities(city_id):
     """ retrieves all places in a city """
-    place = storage.get(City, city_id)
-    if place is None:
+    city = storage.get(City, city_id)
+    if city is None:
         abort(404)
     places = []
-    for i in storage.all('Place').values():
-        if i.city_id == city_id:
-            i.append(place.to_dict())
+    for place in storage.all('Place').values():
+        if place.city_id == city_id:
+            places.append(place.to_dict())
     return jsonify(places)
 
 
-@ app_views.route('/places/<place_id>', methods=['GET'])
+@app_views.route('/places/<place_id>', methods=['GET'])
 def place(place_id):
     """returns place of id given"""
     place = storage.get(Place, place_id)
@@ -32,7 +32,7 @@ def place(place_id):
     return jsonify(place.to_dict()), 200
 
 
-@ app_views.route('/cities/<city_id>/places', methods=['POST'])
+@app_views.route('/cities/<city_id>/places', methods=['POST'])
 def post_place(city_id):
     """create a place and link to city"""
     data = request.get_json(silent=True)
@@ -43,8 +43,7 @@ def post_place(city_id):
     elif 'user_id' not in data.keys():
         return 'Missing user_id', 400
 
-    print(data)
-    if storage.get(User, data['user_id']) is None\
+    if storage.get(User, data['user_id']) is None \
             or storage.get(City, city_id) is None:
         abort(404)
 
@@ -56,7 +55,7 @@ def post_place(city_id):
     return jsonify(new_place.to_dict()), 201
 
 
-@ app_views.route('/places/<place_id>', methods=['PUT'])
+@app_views.route('/places/<place_id>', methods=['PUT'])
 def put_place(place_id):
     """updates given place"""
     find = storage.get(Place, place_id)
@@ -67,15 +66,15 @@ def put_place(place_id):
     if data is None:
         return 'Not a JSON', 400
 
-    for key, values in data.items():
-        if key not in ['id', 'user_id', 'city_id' 'created_at', 'updated_at']:
-            setattr(find, key, values)
+    for key, value in data.items():
+        if key not in ['id', 'user_id', 'city_id', 'created_at', 'updated_at']:
+            setattr(find, key, value)
 
     storage.save()
     return jsonify(find.to_dict()), 200
 
 
-@ app_views.route('/places/<place_id>', methods=['DELETE'])
+@app_views.route('/places/<place_id>', methods=['DELETE'])
 def place_delete(place_id):
     """DELETE place if id is found"""
     place = storage.get(Place, place_id)
